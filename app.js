f33b44c19e57ab4f80150891f8f94cf135d36405
Amitava-Roy <ramitava007@gmail.com>
2023-09-10 20:49:04 +0530

    acess

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "*, http://localhost:5173"
+    "http://localhost:5173"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
