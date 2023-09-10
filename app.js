6cd0dc41ae9a2cf4ae5e7a217139334067c3ec20
Amitava-Roy <ramitava007@gmail.com>
2023-09-10 17:54:17 +0530

    kjkjkjkj

--- app.js
+++ app.js
@@ -45,22 +45,22 @@ app.use(
     fileFilter: fileFilter,
   }).single("image")
 );
-// app.use(
-//   "/images",
-//   express.static(path.join(__dirname, "images"))
-// );
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Content-Type, Authorization"
-//   );
-//   next();
-// });
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "images"))
+);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  next();
+});
 app.use(cors());
 app.use(shopRouter);
 app.use("/admin", adminRouter);
