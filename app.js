e31cf66e29d449bcb63a7014d5f1d1f8414fa0cd
Amitava-Roy <ramitava007@gmail.com>
2023-09-10 20:46:53 +0530

    ok

--- app.js
+++ app.js
@@ -49,7 +49,10 @@ app.use(
   express.static(path.join(__dirname, "images"))
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*, http://localhost:5173"
+  );
   res.setHeader(
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
